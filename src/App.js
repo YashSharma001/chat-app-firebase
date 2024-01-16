39070a63240511d013fb0b7e4188274ca0f98bf3
YashSharma001 <yashsharma.cse20@satyug.edu.in>
2024-01-16 20:16:33 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Box, Button, Container, HStack, Input, VStack, } from "@chakra-ui/react"
+import Message from './Components/Message'
+import { onAuthStateChanged, getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { app } from './firebase'
+import { useEffect, useRef, useState } from "react"
+import { getFirestore, addDoc, collection, serverTimestamp, onSnapshot, query, orderBy } from 'firebase/firestore'
+
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+const loginHandler = () => {
+  const provider = new GoogleAuthProvider();
+
+  signInWithPopup(auth, provider)
+}
+
+const logoutHandler = () => {
+  signOut(auth);
+}
+
+
 
 function App() {
+
+  const [user, setUser] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+
+  const divForScroll = useRef(null);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      setMessage("");
+      await addDoc(collection(db, "Messages"), {
+        text: message,
+        uid: user.uid,
+        uri: user.photoURL,
+        createdAt: serverTimestamp(),
+      })
+
+      divForScroll.current.scrollIntoView({ behavior: "smooth" });
+    } catch (error) {
+      alert(error)
+    }
+
+  }
+
+  useEffect(() => {
+    const q = query(collection(db, "Messages"), orderBy("createdAt", "asc"));
+
+
+    const unsubscribe = onAuthStateChanged(auth, (data) => {
+      setUser(data);
+    });
+
+    const unsubscribeForMessage = onSnapshot(q, (snap) => {
+      setMessages(
+        snap.docs.map((i) => {
+          const id = i.id;
+          return { id, ...i.data() };
+        })
+      );
+    })
+
+    return () => {
+      unsubscribe();
+      unsubscribeForMessage();
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box bg="red.50">
+      {
+        user ? (
+          <Container h="100vh" bg="white">
+            <VStack h={"full"} paddingY={'4'}>
+              <Button onClick={logoutHandler} w={"full"} colorScheme={'red'}>Logout</Button>
+
+              <VStack h={"full"} w={'full'} overflowY={"auto"} css={{
+                "&::-webkit-scrollbar": {
+                  display: "none"
+                }
+              }}>
+                {
+                  messages.map((i) => (
+                    <Message key={i.id} text={i.text} uri={i.uri} user={i.uid === user.uid ? "me" : "other"} />
+                  ))
+                }
+
+                <div ref={divForScroll}></div>
+
+              </VStack>
+
+
+              <form onSubmit={submitHandler} style={{ width: "100%" }}>
+                <HStack>
+                  <Input value={message} onChange={(e) => { setMessage(e.target.value) }} placeholder="Enter a message..." />
+                  <Button colorScheme={'purple'} type="submit">Send</Button>
+                </HStack>
+              </form>
+
+            </VStack>
+          </Container>
+        ) : <VStack bg={"white"} h={"100vh"} justifyContent={"center"}>
+          <Button onClick={loginHandler} colorScheme="purple">Sign in with Google</Button>
+        </VStack>
+      }
+    </Box>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBlaji384dG3pxaL1uAcXaGfwyujHbvREU",
+    authDomain: "mychatapp-66a2d.firebaseapp.com",
+    projectId: "mychatapp-66a2d",
+    storageBucket: "mychatapp-66a2d.appspot.com",
+    messagingSenderId: "941156339631",
+    appId: "1:941156339631:web:1db83d153341121992295a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
